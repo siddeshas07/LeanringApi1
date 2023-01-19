a2059f27dcf04a82122ee11d2ca0d4ce7c313a9b
siddeshas07 <58839558+siddeshas07@users.noreply.github.com>
2023-01-19 18:11:03 +0530

    Initial commit

---
+++ src/main/java/com/Learning/api/LeanringApi/LeanringApiApplication.java
@@ -0,0 +1,13 @@
+package com.Learning.api.LeanringApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Learning/api/LeanringApi/LeanringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learning.api.LeanringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
